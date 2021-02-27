ac53c52abd902721a95aa69b217dfe3bcaf2b5ae
Doan Manh <dvmanh96vp@gmail.com>
2021-02-27 12:49:08 +0700

    Form Builder

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,6 @@
 import { CustomTextValidator } from './custom-text.validator';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,6 +8,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+  subcribeFormFb;
   subcribeForm = new FormGroup({
     // gom group
     contact: new FormGroup({
@@ -16,9 +17,22 @@ export class ReactiveFormComponent implements OnInit {
     period: new FormControl(''),
     news: new FormControl()
   });
-  constructor() { }
+  // dùng form builder
+  // constructor(private fb: FormBuilder) {
+  //   this.subcribeFormFb = fb.group({
+  //     contact: fb.group({
+  //       email: ['', [Validators.required, Validators.minLength(3), CustomTextValidator.specialCharacters]]
+  //     }),
+  //     period: [''],
+  //     news: []
+  //   })
+  // }
+  constructor() {
+
+  }
 
   get(name) {
+    // return this.subcribeFormFb['controls']['contact']['controls'][name]; dùng form builder
     return this.subcribeForm['controls']['contact']['controls'][name];
   }
   ngOnInit(): void {
