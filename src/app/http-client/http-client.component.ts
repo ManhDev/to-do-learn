8913fe7257e4b49a8f93401d502b764e54759f31
Doan Manh <dvmanh96vp@gmail.com>
2021-03-06 09:58:19 +0700

    Change to Observable

--- src/app/http-client/http-client.component.ts
+++ src/app/http-client/http-client.component.ts
@@ -1,5 +1,7 @@
+import { FormControl } from '@angular/forms';
 import { SearchService } from './search.service';
 import { Component, OnInit } from '@angular/core';
+import {debounceTime, filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-http-client',
@@ -7,18 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./http-client.component.css']
 })
 export class HttpClientComponent implements OnInit {
-  searchValue: string = '';
+  searchValue= new FormControl();
   results = [];
   constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
-  }
+    // this.searchValue.valueChanges.pipe(
+    //   filter((valuaInput)=>valuaInput!==''),
+    //   debounceTime(500)
+    // )
+    // .subscribe((value)=>{
+    //   this.searchService.getInfo(value).then((info)=>{
+    //     this.results = info[1]
+    //   })
+    // })
+    // pipe Là tạo đường dẫn stream: chứa các tiền xử lý
+    // các filter, debounceTime là tiền xử lý là lọc và delay 500ms
+    // subscribe là khi xong tất cả mọi thứ ở trong pipe(luồng) và đã được tiền xử lý
 
-  doSearch() {
-    this.searchService.getInfo(this.searchValue).then(info => {
-     this.results = info[1]
+
+    let observable = this.searchValue.valueChanges;
+    let observer_1 = observable.subscribe(value => {
+      console.log("observer_1: ", value);
+      
+    })
+    let observer_2 = observable.subscribe(value => {
+      console.log("observer_2: ", value);
       
     })
   }
-
 }
